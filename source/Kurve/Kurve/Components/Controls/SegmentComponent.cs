9c9d305ff9369cfb385633638f138c82f687e285
Julian Brunner <julianbrunner@gmail.com>
2014-01-26 17:30:56 +0100

    added curvature visualization, improved discrete curve resolution

--- source/Kurve/Kurve/Components/Controls/SegmentComponent.cs
+++ source/Kurve/Kurve/Components/Controls/SegmentComponent.cs
@@ -41,7 +41,7 @@ namespace Kurve.Component
 		{
 			if (Curve == null) return;
 
-			foreach (Tuple<double, double> positions in Scalars.GetIntermediateValuesSymmetric(leftComponent.Position, rightComponent.Position, 100).GetRanges()) 
+			foreach (Tuple<double, double> positions in Scalars.GetIntermediateValuesSymmetric(leftComponent.Position, rightComponent.Position, 250).GetRanges()) 
 			{
 				double stretchFactor = Curve.GetSpeed((positions.Item1 + positions.Item2) / 2) / BasicSpecification.CurveLength;
 
@@ -50,12 +50,13 @@ namespace Kurve.Component
 				Krach.Graphics.Color color = StretchedColor(stretchFactor);
 				Drawing.DrawLine(context, Curve.GetPoint(positions.Item1), Curve.GetPoint(positions.Item2), 2, color);
 
-//				Vector2Double point = 0.5 * (Curve.GetPoint(positions.Item1) + Curve.GetPoint(positions.Item2));
-//				Vector2Double direction = Curve.GetDirection((positions.Item1 + positions.Item2) / 2);
-//				Vector2Double angularDirection = new Vector2Double(direction.Y, -direction.X);
-//				double curvature = Curve.GetCurvature((positions.Item1 + positions.Item2) / 2);
-//				Vector2Double curvatureVector = 10000 * curvature * angularDirection;
-//				Drawing.DrawLine(context, point, point + curvatureVector, 2, Colors.Green);
+				Vector2Double point = 0.5 * (Curve.GetPoint(positions.Item1) + Curve.GetPoint(positions.Item2));
+				double direction = Curve.GetDirection((positions.Item1 + positions.Item2) / 2);
+				Vector2Double directionVector = new Vector2Double(Scalars.Cosine(direction), Scalars.Sine(direction));
+				Vector2Double angularDirection = new Vector2Double(directionVector.Y, -directionVector.X);
+				double curvature = Curve.GetCurvature((positions.Item1 + positions.Item2) / 2);
+				Vector2Double curvatureVector = 10000 * curvature * angularDirection;
+				Drawing.DrawLine(context, point, point + curvatureVector, 0.5, Colors.Green);
 			}
 
 			base.Draw(context);
